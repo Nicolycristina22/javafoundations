04f45054ddbb17074aa7db90519ffa1a8bc406d1
Nicoly Cristina <160439544+Nicolycristina22@users.noreply.github.com>
2025-12-10 13:01:53 -0300

    Atualizar o menu.java

--- aula6/AlunosCrud/src/Lanches/menu.java
+++ aula6/AlunosCrud/src/Lanches/menu.java
@@ -6,21 +6,20 @@ public class menu {
     private ArrayList<Pedido> lanche = new ArrayList<>();
 
     public menu() {
-        lanche.add(new Pedido("x tudo", 30,6));
-        lanche.add(new Pedido("x bacon", 40,6));
-        lanche.add(new Pedido("x egg", 39.90,6));
-        lanche.add(new Pedido("x franco", 29.90,6));
-
-
+        lanche.add(new Pedido("x tudo", 30, 6));
+        lanche.add(new Pedido("x bacon", 40, 6));
+        lanche.add(new Pedido("x egg", 39.90, 6));
+        lanche.add(new Pedido("x franco", 29.90, 6));
     }
+
     public void exibirLanches() {
         System.out.println("-----------menu lanches ----------------");
-            for (int i =0; i<lanche.size();i++) {
-                System.out.println(i+ lanche.get(i).toString());
-            }
-        }
-        public Pedido getLanche (int id){
-        return lanche.get(id);
+        for (int i = 0; i < lanche.size(); i++) {
+            System.out.println(i + " - " + lanche.get(i).toString());
         }
     }
 
+    public Pedido getLanche(int id) {
+        return lanche.get(id);
+    }
+}
